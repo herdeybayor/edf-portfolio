11c4332430facdd59311864ca330c1a02a6b6fb5
EmekadeFirst <119001445+emekadefirst@users.noreply.github.com>
2023-09-13 00:04:45 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&eix))c8%)f-3b&*osjh_&9oedzcejmkp_a*69(cfz&l$=y7qi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
